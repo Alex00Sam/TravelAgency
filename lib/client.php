cc99be776498a784588612715ed00e652c01c87b
Alex00Sam <alsa00@mail.ru>
2017-06-14 15:14:24 +0300

    Create client.php

--- lib/client.php
+++ lib/client.php
@@ -3,6 +3,9 @@ class client extends \atk4\data\Model {
 	public $table = 'client';	
 	function init() {
 		parent::init();
-		$this->addFields(['name','surname','email','destination']);
+		$this->addField('name', ['required'=>true]);
+		$this->addField('surname', ['required'=>true]);
+		$this->addField('email', ['required'=>true]);
+		$this->addField('destination', ['required'=>true]);
 	}
 }
