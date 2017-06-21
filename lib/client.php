7f3efed58416e882b08dcb13c07b106bb84e7676
Alex00Sam <alsa00@mail.ru>
2017-06-21 11:50:01 +0300

    Update client.php

--- lib/client.php
+++ lib/client.php
@@ -7,6 +7,6 @@ class client extends \atk4\data\Model {
 		$this->addField('surname', ['required'=>true]);
 		$this->addField('email', ['required'=>true]);
 		$this->addField('destination', ['required'=>true]);
-		$this->addField('feedback', ['required'=>true]);
+		$this->addField('feedback', ['type'=>'text','required'=>true]);
 	}
 }
